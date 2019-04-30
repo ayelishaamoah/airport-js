7d3acccc4e190e88dc5dede906799354cd2a086a
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-04-30 14:52:33 +0100

    Added takeoff method and test

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -7,7 +7,14 @@ describe('Airport', function(){
 
   describe('land', function() {
     it('allows a plane to land', function(){
-      expect(airport.land(plane)).toContain(plane)
+      expect(airport.land(plane)).toContain(plane);
+    });
+  });
+
+  describe('takeOff', function() {
+    it('allows a plane to take off', function(){
+      airport.land(plane);
+      expect(airport.takeOff(plane)).not.toContain(plane);
     });
   });
 });
--- src/airport.js
+++ src/airport.js
@@ -1,11 +1,17 @@
 function Airport() {
-  this.hangar = []
+  this.hangar = [];
 }
 
 function Plane() {
 }
 
-Airport.prototype.land = function (plane) {
+Airport.prototype.land = function(plane) {
   this.hangar.push(plane)
   return this.hangar;
-};
+}
+
+Airport.prototype.takeOff = function(plane) {
+  var index = this.hangar.indexOf(plane);
+  this.hangar.splice(index, 1);
+  return this.hangar;
+}
