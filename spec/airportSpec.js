643f32249760184bd4eac4a5e89f28ad0e4bc9e1
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-05-01 15:01:55 +0100

    refactored to be inline with walkthrough

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,38 +1,17 @@
+'use strict';
+
 describe('Airport', function(){
-
-  beforeEach(function() {
-    weather = new Weather();
-    airport = new Airport(weather);
-    plane = new Plane();
+  var airport;
+  var plane;
+  beforeEach(function(){
+    airport = new Airport();
+    plane = jasmine.createSpy('plane',['land']);
   });
-
-  describe('land', function() {
-    it('allows a plane to land', function(){
-      spyOn(weather,'_getRandomWeather').and.returnValue(2);
-      expect(airport.land(plane)).toContain(plane);
-    });
-
-    it('prevents landing in stormy weather', function() {
-      spyOn(weather,'_getRandomWeather').and.returnValue(8);
-      expect(function() {airport.land(plane)}).toThrowError("Can't land in stormy weather");
-    });
+  it('has no planes by default', function(){
+    expect(airport.planes()).toEqual([]);
   });
-
-  describe('takeOff', function() {
-    it('allows a plane to take off', function(){
-      spyOn(weather,'_getRandomWeather').and.returnValue(2);
-
-      airport.land(plane);
-      expect(airport.takeOff(plane)).not.toContain(plane);
-    });
-
-    it('prevents take off in stormy weather', function() {
-      airport.hangar.push(plane);
-
-      spyOn(weather,'_getRandomWeather').and.returnValue(8);
-
-      expect(function() { airport.takeOff(plane)}).toThrowError("Can't take off in stormy weather");
-    });
+  it('can clear planes for landing', function(){
+    airport.clearForLanding(plane);
+    expect(airport.planes()).toEqual([plane]);
   });
-
 });
--- src/airport.js
+++ src/airport.js
@@ -1,47 +1,9 @@
-function Weather() {
+'use strict';
+
+function Airport(){
+  this._hangar = [];
 }
-
-function Airport(weather) {
-  this.hangar = [];
-  this.weather = weather;
-}
-
-function Plane() {
-}
-
-
-Weather.prototype._getRandomWeather = function(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-Airport.prototype.land = function(plane) {
-  if (this._isStormy() == true) {
-    throw new Error("Can't land in stormy weather");
-  } else {
-    this.hangar.push(plane)
-    return this.hangar;
-  }
-}
-
-Airport.prototype.takeOff = function(plane) {
-  if (this._isStormy() == true) {
-    throw new Error("Can't take off in stormy weather");
-  } else {
-    var index = this.hangar.indexOf(plane);
-    this.hangar.splice(index, 1);
-    return this.hangar;
-  }
-}
-
-Airport.prototype._isStormy = function() {
-  var STORMYLIMIT = 7
-  var currentWeather = this.weather._getRandomWeather(1, 10)
-
-  if (currentWeather > STORMYLIMIT) {
-    return true
-  } else {
-    return false
-  }
-}
+Airport.prototype.planes = function(){ return this._hangar; };
+Airport.prototype.clearForLanding = function(plane) {
+  this._hangar.push(plane);
+};
